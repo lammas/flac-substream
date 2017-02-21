466128ec120b814612344be812927692aa55eb3b
lammas <lammas@gmail.com>
2017-02-21 21:08:15 +0200

    Add: simple file access test

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const test = require('tape');
+const fs = require('fs');
+const devnull = require('dev-null');
+const seektable = require('flac-seektable');
 
 const bitutils = require('../src/bitutils');
+const substream = require('../index');
 
 test('bitutils: uintToBits / uintFromBits', function(t) {
 	const value = 123;
@@ -23,4 +27,34 @@ test('bitutils: bufferToBits / bitsToBuffer', function(t) {
 	var reverse = bitutils.bitsToBuffer(bits);
 	t.deepEquals(reverse, src, 'Reverse conversion OK');
 	t.end();
+});
+
+test('Plain file random access', function(t) {
+	const FLAC_IN = 'test/nyanya.flac';
+	const FLAC_OUT = 'test/out.flac';
+	const START_SECONDS = 2;
+	const END_SECONDS = 5;
+
+	t.plan(2);
+
+	var reader = fs.createReadStream(FLAC_IN);
+	reader.pipe(new seektable(function(data) {
+		t.ok(data, 'Got seek data');
+		var output = fs.createWriteStream(FLAC_OUT, { flags: 'w' });
+		output.on('finish', () => {
+			t.pass('Write stream finished');
+			t.end();
+		});
+
+		substream(output, data, START_SECONDS, END_SECONDS, function(start, end) {
+			var stream = fs.createReadStream(
+				FLAC_IN,
+				{
+					start: start,
+					end: end
+				}
+			);
+			return stream;
+		});
+	})).pipe(devnull());
 });
